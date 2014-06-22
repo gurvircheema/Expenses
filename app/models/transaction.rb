82d814500da657ea7de10fdca6d73d04e5d7ec36
GurvirCheema <GurvirCheema@hotmail.com>
2014-06-22 12:18:07 -0700

    Added additional validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :category
+
+	validates_presence_of :description
+	validates_presence_of :amount
+	validates_associated :user
 end
